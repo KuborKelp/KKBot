7f8d1345c16c4f26cbf0bf54736c307bcd2e6a2c
KuborKelp <2321247175@qq.com>
2023-11-04 10:40:42 +0800

    Initial commit

--- main.py
+++ main.py
@@ -2,6 +2,29 @@ import public.datebase as datebase
 import public.message as message
 import public.parser as parser
 import os
+import importlib
+import sys
+
+PATH_MODULES = "./modules/"
+headers = {}
+s_headers = {}
+modules = {}
+
+
+def load_modules():
+    sys.path.append(PATH_MODULES)
+    mods = os.listdir(PATH_MODULES)
+    mods = [i[:-3] for i in mods if i.endswith(".py")]
+    for mod in mods:
+        modules[mod] = __import__("kelpium")
+        info = modules["kelpium"]._info()
+        headers[mod] = info[0]
+        s_headers[mod] = info[1]
+
+    print(mods)
+
 
 if __name__ == '__main__':
-    print(1111)
+    load_modules()
+    print(headers)
+    print(s_headers)
