c8b7a0421e65de0ff959237f5882c45b20271dc5
KuborKelp <2321247175@qq.com>
2023-11-04 10:39:26 +0800

    Initial commit

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+import public.datebase as datebase
+import public.message as message
+import public.parser as parser
+import os
+
 if __name__ == '__main__':
     print(1)
--- public/datebase.py
+++ public/datebase.py
@@ -1,5 +1,5 @@
-import os
 import sqlite3
+import os
 
 PATH = "./database/"
 
