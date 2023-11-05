2f4dd4b72468ba4b4757d7bb1f34afaf70f9b9d6
KuborKelp <2321247175@qq.com>
2023-11-05 09:46:20 +0800

    bot.py

--- modules/kelpium.py
+++ modules/kelpium.py
@@ -156,6 +156,6 @@ def get_kelpium_ranking(group_info):
     result = "=====KELPIUM-RANKING=====\n KELPIUM   NAME\n"
     for r in ranking:
         kelpium_len = len(str(r[1]))
-        result += str(r[1]) + " " * (9 - kelpium_len) + "  " + r[0] + "\n"
+        result += str(r[1]) + " " * (12 - kelpium_len) + "  " + r[0] + "\n"
     result += "========================="
     return result
